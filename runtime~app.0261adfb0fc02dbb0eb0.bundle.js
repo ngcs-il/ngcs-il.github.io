c7fd40192c4afe922f21d70f2067503e61ed4c97
David Kossoglyad <david@logoui.co.il>
2020-07-26 08:58:38 +0300

    Deploy updates

--- runtime~app.0261adfb0fc02dbb0eb0.bundle.js
+++ runtime~app.0261adfb0fc02dbb0eb0.bundle.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "" + ({"vendors.async~3e799143":"vendors.async~3e799143"}[chunkId]||chunkId) + "." + "f794049358547414da24" + ".chunk.js"
+/******/ 		return __webpack_require__.p + "" + ({"vendors.async~3e799143":"vendors.async~3e799143"}[chunkId]||chunkId) + "." + "0261adfb0fc02dbb0eb0" + ".chunk.js"
 /******/ 	}
 /******/
 /******/ 	// The require function
