e597961127744fa4d1f081ea6ed6c9d447ce1c47
David Kossoglyad <david@logoui.co.il>
2020-08-02 10:23:27 +0300

    chore: publish

--- runtime~app.25ba513ff26ec5c12f18.bundle.js
+++ runtime~app.25ba513ff26ec5c12f18.bundle.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "" + ({"vendors.async~3e799143":"vendors.async~3e799143"}[chunkId]||chunkId) + "." + "a47e282f97eaf854fb3f" + ".chunk.js"
+/******/ 		return __webpack_require__.p + "" + ({"vendors.async~3e799143":"vendors.async~3e799143"}[chunkId]||chunkId) + "." + "25ba513ff26ec5c12f18" + ".chunk.js"
 /******/ 	}
 /******/
 /******/ 	// The require function
